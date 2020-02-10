4edb763cd17084ac6c42b49cc57c38b2e17d07ce
n1908 <n19008@std.it-college.ac.jp>
2020-02-10 12:36:22 +0900

    フォーム

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React from 'react'
 
-class Appp extends React.Component {
-    constructor (props) {
-      super(props)
-      this.state = {}
-    }
-    render () {
-      return (
-        <div>
-        </div>
+const TestView = props => <div>{props.value}</div>
+
+class TestForm extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { value: '' }
+  }
+
+  handleChange (event) {
+    this.setState({ value: event.target.value })
+  }
+
+  render () {
+    return (
+      <form onSubmit={(e, v) => this.props.update(e, this.state.value)}>
+        <input type='text' onChange={e => this.handleChange(e)} />
+        <input type='submit' />
+      </form>
     )
   }
-  componentDidMount () {}
+}
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { value: 'first' }
+  }
+
+  handleSubmit (e, value) {
+    e.preventDefault()
+    this.setState({ value: value })
+  }
+  render () {
+    return (
+      <div>
+        <TestView value={this.state.value} />
+        <TestForm update={this.handleSubmit.bind(this)} />
+      </div>
+    )
+  }
 }
 
 export default App
