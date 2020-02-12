36c3ce29a9ad67723856f6ce0d88f1f1b06efb9f
n1908 <n19008@std.it-college.ac.jp>
2020-02-12 14:15:15 +0900

    sample

--- src/App.js
+++ src/App.js
@@ -1,42 +1,16 @@
 import React from 'react'
-
-const TestView = props => <div>{props.value}</div>
-
-class TestForm extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { value: '' }
-  }
-
-  handleChange (event) {
-    this.setState({ value: event.target.value })
-  }
-
-  render () {
-    return (
-      <form onSubmit={(e, v) => this.props.update(e, this.state.value)}>
-        <input type='text' onChange={e => this.handleChange(e)} />
-        <input type='submit' />
-      </form>
-    )
-  }
-}
+import InchToCm from './InchToCm'
 
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = { value: 'first' }
+    this.state = {}
   }
 
-  handleSubmit (e, value) {
-    e.preventDefault()
-    this.setState({ value: value })
-  }
   render () {
     return (
       <div>
-        <TestView value={this.state.value} />
-        <TestForm update={this.handleSubmit.bind(this)} />
+        <InchToCm />
       </div>
     )
   }
