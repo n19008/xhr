987e8a15fceb7025d6a84630214904066e8a8f6c
n1908 <n19008@std.it-college.ac.jp>
2020-02-18 16:45:46 +0900

    sample2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react'
+import axios from 'axios'
+//import ReactDOM from 'react-dom'
+const url = 'https://holidays-jp.github.io/api/v1/date.json'
+const url2 =
+  'http://weather.livedoor.com/forecast/webservice/json/v1?city=400040'
 
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      //isLoading: false
+    }
   }
+    handleClick = () => {
+        axios
+            .get(url2)
+            .then(res => console.log(res))
+    }
 
   render () {
-    return (
-      <div>
-      </div>
-    )
+    // if (this.isLoading) {
+    //return <p>loading...</p>
+    // }
+      return (
+          <div>
+          <button onClick = {this.handleClick}>wa-i</button>
+          </div>
+      )
   }
-  componentDidMount () {}
 }
+
 export default App
