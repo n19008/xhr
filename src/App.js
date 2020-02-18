016c7473ea4ea6c30878ee40459189ddf8aaafac
n1908 <n19008@std.it-college.ac.jp>
2020-02-18 17:44:25 +0900

    sample

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React from 'react'
+//import ReactDOM from 'react-dom'
+const url = 'https://holidays-jp.github.io/api/v1/date.json'
+const url2 =
+    'http://weather.livedoor.com/forecast/webservice/json/v1?city=400040'
 
 class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {}
-  }
+    constructor (props) {
+        super(props)
+        this.state = {
+            //isLoading: false
+        }
+    }
+    componentDidMount() {
+        fetch(url)
+            .then((res) => {
+                this.setState({
+                    isLoding : true
+                })
+                console.log(res)
+                return res.json()
+            })
+            .then((data) => {
+                console.log(data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
 
-  render () {
-    return (
-      <div>
-      </div>
-    )
-  }
-  componentDidMount () {}
+    render () {
+        //if (this.isLoading) {
+        //return <p>loading...</p>
+        //}
+        return <div></div>
+    }
 }
+
 export default App
