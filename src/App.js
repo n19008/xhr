11995d3dd62729758b87e4ec1db8f89a240c3914
n1908 <n19008@std.it-college.ac.jp>
2020-02-20 00:43:36 +0900

    画像縮小

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './main.css'
 //import ReactDOM from 'react-dom'
 const url = "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?format=json&formatVersion=1&applicationId=1025199417506477360"
 
@@ -6,41 +7,26 @@ class App extends React.Component {
     constructor (props) {
         super(props)
         this.state = {
-            //isLoading: false
-            images : '' ,
             data1 : null,
-            formTxt : ''
         }
     }
 
     handleClick = () => {
         fetch(url)
             .then((res) => {
-                //this.setState({
-                //isLoding : true
-                //})
-                //console.log(res)
                 return res.json()
             })
             .then((data) => {
-                //オブジェクトがreslut
-                //console.log(data['result'][0])
-                //const v = data['result'][0]
-                //console.log(v['recipeDescription'])
-                //画像パス
-                //const image = v['foodImageUrl']
                 console.log(data)
                 this.setState ({
-                    // images : image,
                     data1 : data
-                    // data1 : v
                 })
                 console.log(JSON.stringify(this.state.data1['result'][0]['foodImageUrl']))
                 //   console.log(this.state.images)
             })
-        //   .catch((err) => {
-        //       console.log(err)
-        //   })
+           .catch((err) => {
+               console.log(err)
+           })
     }
 
     render () {
@@ -52,9 +38,9 @@ class App extends React.Component {
             ur = this.state.data1['result'][0]['foodImageUrl']
         }
         return (
-            <div>
+            <div className='imagesource'>
                 <button onClick = {this.handleClick}>wa-i</button>
-                <p>{ur}</p>
+                <img src = {ur} alt=''/>
             </div>
         )
 
